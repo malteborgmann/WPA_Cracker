92352694f69a52a16b451a8d21cc835413e5c099
Malte Borgmann <malte@borgmann.tech>
2024-09-21 23:07:15 +0800

    Better and working structure

--- main.c
+++ main.c
@@ -2,25 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE_MAC 18
+#define SIZE_ESSENTIALS 33
+#define SIZE_EAPOL 68 // Arbitrary size for EAPOL
 
-/*
-WPA*01*PMKID*MAC_AP*MAC_CLIENT*ESSID***MESSAGEPAIR
-WPA*02*MIC*MAC_AP*MAC_CLIENT*ESSID*NONCE_AP*EAPOL_CLIENT*MESSAGEPAIR
-https://hashcat.net/wiki/doku.php?id=cracking_wpawpa2
-*/
-
-/*
- * Actually we only need PMKID, MAC_AP, MAC_CLIENT, ESSID
- */
-
-struct wpa{
+struct wpa_hash {
     struct {
         char *pmkid;
         char *mac_ap;
         char *mac_client;
         char *essid;
-        char *messagepair;
     } wpa1;
+
     struct {
         char *mic;
         char *mac_ap;
@@ -28,55 +21,114 @@ struct wpa{
         char *essid;
         char *nonce_ap;
         char *eapol_client;
-        char *messagepair;
     } wpa2;
 };
 
-void print_info(struct wpa *info) {
-    printf("Data received from 22000 file: \n");
-    printf("pmkid: %s\n", info->wpa1.pmkid);
-    printf("ssid: %s\n", info->wpa1.essid);
-    printf("mac_client: %s\n", info->wpa1.mac_client);
-    printf("mac_ap: %s\n", info->wpa1.mac_ap);
-    printf("\n");
-}
+struct wpa_hash get_input(char *filepath) {
+    struct wpa_hash hash;
 
-void get_input(char *filename) {
+    // Allocate memory for WPA1 fields
+    hash.wpa1.pmkid = malloc(SIZE_ESSENTIALS);
+    hash.wpa1.mac_ap = malloc(SIZE_MAC);
+    hash.wpa1.mac_client = malloc(SIZE_MAC);
+    hash.wpa1.essid = malloc(SIZE_ESSENTIALS);
 
-}
+    // Allocate memory for WPA2 fields
+    hash.wpa2.mic = malloc(SIZE_ESSENTIALS);
+    hash.wpa2.mac_ap = malloc(SIZE_MAC);
+    hash.wpa2.mac_client = malloc(SIZE_MAC);
+    hash.wpa2.essid = malloc(SIZE_ESSENTIALS);
+    hash.wpa2.nonce_ap = malloc(SIZE_ESSENTIALS);
+    hash.wpa2.eapol_client = malloc(SIZE_EAPOL);
 
-
-
-int main(void) {
-    FILE *fptr = fopen("./data/input.hc22000", "r");
+    FILE *fptr = fopen(filepath, "r");
     if (fptr == NULL) {
+        perror("Error opening file");
         exit(EXIT_FAILURE);
     }
     printf("File opened successfully\n");
 
-    char * line = NULL;
+    char *line = NULL;
     size_t len = 0;
     ssize_t wpa;
 
     while ((wpa = getline(&line, &len, fptr)) != -1) {
         printf("Retrieved line of length %zu:\n", wpa);
-        printf("%s", line);
+        printf("%s\n", line);
 
         char delimiter[] = "*";
         char *ptr;
 
-        // initialisieren und ersten Abschnitt erstellen
         ptr = strtok(line, delimiter);
-
-        while(ptr != NULL) {
-            printf("Abschnitt gefunden: %s\n", ptr);
-            // naechsten Abschnitt erstellen
-            ptr = strtok(NULL, delimiter);
+        ptr = strtok(NULL, delimiter);
+        if (ptr != NULL) {
+            if (strcmp(ptr, "01") == 0) {  // WPA1 (PMKID)
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa1.pmkid, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa1.mac_ap, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa1.mac_client, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa1.essid, ptr);
+            } else if (strcmp(ptr, "02") == 0) {  // WPA2 (MIC)
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa2.mic, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa2.mac_ap, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa2.mac_client, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa2.essid, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa2.nonce_ap, ptr);
+                ptr = strtok(NULL, delimiter);
+                strcpy(hash.wpa2.eapol_client, ptr);
+            }
         }
     }
+
     fclose(fptr);
     if (line) {
         free(line);
     }
+
+    return hash;
+}
+
+int main(void) {
+    char filepath [] = "./data/input.hc22000";
+    struct wpa_hash hash = get_input(filepath);
+
+    // Output WPA1 data if present
+    printf("\nWPA1 Data:\n");
+    printf("PMKID: %s\n", hash.wpa1.pmkid);
+    printf("MAC AP: %s\n", hash.wpa1.mac_ap);
+    printf("MAC Client: %s\n", hash.wpa1.mac_client);
+    printf("ESSID: %s\n", hash.wpa1.essid);
+
+    // Output WPA2 data if present
+    printf("\nWPA2 Data:\n");
+    printf("MIC: %s\n", hash.wpa2.mic);
+    printf("MAC AP: %s\n", hash.wpa2.mac_ap);
+    printf("MAC Client: %s\n", hash.wpa2.mac_client);
+    printf("ESSID: %s\n", hash.wpa2.essid);
+    printf("Nonce AP: %s\n", hash.wpa2.nonce_ap);
+    printf("EAPOL Client: %s\n", hash.wpa2.eapol_client);
+
+    // Free allocated memory for WPA1
+    free(hash.wpa1.pmkid);
+    free(hash.wpa1.mac_ap);
+    free(hash.wpa1.mac_client);
+    free(hash.wpa1.essid);
+
+    // Free allocated memory for WPA2
+    free(hash.wpa2.mic);
+    free(hash.wpa2.mac_ap);
+    free(hash.wpa2.mac_client);
+    free(hash.wpa2.essid);
+    free(hash.wpa2.nonce_ap);
+    free(hash.wpa2.eapol_client);
+
     return 0;
 }
