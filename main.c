6c8db16a7547bd7a179606975aac41dc4155e010
Malte Borgmann <malte@borgmann.tech>
2024-10-16 17:30:10 +0800

    Reading from hc22000 file

--- main.c
+++ main.c
@@ -7,15 +7,6 @@
 #define MAX_LINE 1024
 #define MAX_PASSLIST 1000000
 
-// Default Values
-
-char *pmkid = "4d4fe7aac3a2cecab195321ceb99a7d0";
-unsigned char essid[] = "hashcat-essid";
-unsigned char mac_ap[] = {0xfc, 0x69, 0x0c, 0x15, 0x82, 0x64};
-unsigned char mac_cl[] = {0xf4, 0x74, 0x7f, 0x87, 0xf9, 0xf4};
-char *passlist_src = "passlist.txt";
-
-
 void hex_to_bytes(const char *hex, unsigned char *bytes, size_t len) {
     for (size_t i = 0; i < len; i++) {
         sscanf(hex + 2*i, "%2hhx", &bytes[i]);
@@ -26,14 +17,14 @@ void print_hex(unsigned char *data, int len) {
     for (int i = 0; i < len; i++) {
         printf("%02x", data[i]);
     }
+    printf("\n");
 }
 
-int crack_pmkid(const char *pmkid, const unsigned char *essid, const unsigned char *mac_ap,
+int crack_pmkid(const unsigned char *pmkid, const unsigned char *essid, const unsigned char *mac_ap,
                 const unsigned char *mac_cl, char **passlist, int passlist_len) {
     unsigned char pmk[32];
     unsigned char message[23] = "PMK Name";
-    unsigned char hmac_result[20];
-    char hmac_hex[41];
+    unsigned char hmac_result[32];
     unsigned int hmac_len;
 
     memcpy(message + 8, mac_ap, 6);
@@ -44,41 +35,89 @@ int crack_pmkid(const char *pmkid, const unsigned char *essid, const unsigned ch
 
         HMAC(EVP_sha1(), pmk, 32, message, 20, hmac_result, &hmac_len);
 
-        for (int j = 0; j < 16; j++) {
-            sprintf(hmac_hex + (j * 2), "%02x", hmac_result[j]);
-        }
-        hmac_hex[32] = '\0';
-
-        printf("%s\n", hmac_hex);
-
-        if (strncmp(hmac_hex, pmkid, 32) == 0) {
-            printf("\033[92m%s - Matches captured PMKID\n\n", hmac_hex);
-            printf("Password Cracked!\n\033[0m\n");
+        if (memcmp(hmac_result, pmkid, 16) == 0) {
+            printf("\033[92mPassword Cracked!\033[0m\n");
             printf("SSID:             %s\n", essid);
-            printf("Password:         %s\n\n", passlist[i]);
+            printf("Password:         %s\n", passlist[i]);
             return 1;
         }
     }
     return 0;
 }
 
+int read_hc22000(const char *filename, unsigned char *pmkid, unsigned char *essid, unsigned char *mac_ap, unsigned char *mac_cl) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Could not open file %s\n", filename);
+        return 0;
+    }
+
+    char line[MAX_LINE];
+    if (fgets(line, sizeof(line), file) != NULL) {
+        char *token = strtok(line, "*");
+        if (token != NULL && strcmp(token, "WPA") == 0) {
+            token = strtok(NULL, "*");
+            token = strtok(NULL, "*");
+            printf("Token: %s\n", token);
+            if (token != NULL) {
+                hex_to_bytes(token, pmkid, 16);
+            }
+            token = strtok(NULL, "*");
+            if (token != NULL) {
+                hex_to_bytes(token, mac_ap, 6);
+            }
+            token = strtok(NULL, "*");
+            if (token != NULL) {
+                hex_to_bytes(token, mac_cl, 6);
+            }
+            token = strtok(NULL, "*");
+            if (token != NULL) {
+                strncpy((char*)essid, token, strlen(token));
+                essid[strlen(token)] = '\0';
+            }
+        }
+    }
+
+    fclose(file);
+    return 1;
+}
+
 int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <hc22000_file> <passlist_file>\n", argv[0]);
+        return 1;
+    }
+
+    char *hc22000_file = argv[1];
+    char *passlist_file = argv[2];
+
+    unsigned char pmkid[16];
+    unsigned char essid[MAX_LINE];
+    unsigned char mac_ap[6];
+    unsigned char mac_cl[6];
+
+    if (!read_hc22000(hc22000_file, pmkid, essid, mac_ap, mac_cl)) {
+        return 1;
+    }
+
+    printf("Read from hc22000 file:\n");
+    printf("PMKID: ");
+    print_hex(pmkid, 16);
+    printf("ESSID: %s\n", essid);
+    printf("MAC AP: ");
+    print_hex(mac_ap, 6);
+    printf("MAC Client: ");
+    print_hex(mac_cl, 6);
+
     char *passlist[MAX_PASSLIST];
     int passlist_len = 0;
     char line[MAX_LINE];
     FILE *file;
 
-    // User Supplied Values
-    if (argc > 1) pmkid = argv[1];
-    if (argc > 2) strcpy((char*)essid, argv[2]);
-    if (argc > 3) hex_to_bytes(argv[3], mac_ap, 6);
-    if (argc > 4) hex_to_bytes(argv[4], mac_cl, 6);
-    if (argc > 5) passlist_src = argv[5];
-
-    // Read passlist.txt
-    file = fopen(passlist_src, "r");
+    // Read passlist file
+    file = fopen(passlist_file, "r");
     if (file == NULL) {
-        printf("Could not open file %s\n", passlist_src);
+        printf("Could not open file %s\n", passlist_file);
         return 1;
     }
 
